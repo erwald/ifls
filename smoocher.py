a1e0a95bc52af83b2197a1422fd39ce6092e846c
Erich Grunewald <erich.grunewald@futurice.com>
2019-06-24 09:57:40 +0200

    Don't make comments

--- smoocher.py
+++ smoocher.py
@@ -40,10 +40,6 @@ with smart_run(session):
              'latinamericanfotografia', 'aklphotofestival', 'mfonfoto']
     session.set_user_interact(amount=4, randomize=True,
                               percentage=50, media='Photo')
-    emojis = [':fire:', ':raised_hands:', ':clap:', ':boom:',
-              ':dizzy:', ':sparkles:', ':gem:', ':ok_hand:', ':100:']
-    session.set_comments([f"emoji" for emoji in emojis])
-    session.set_do_comment(True, percentage=20)
     session.set_do_like(True, percentage=70)
     session.set_do_follow(True, percentage=100)
     session.interact_user_followers(users, amount=10, randomize=True)
